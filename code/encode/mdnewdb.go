d9bfd4b6da3c1545ca227d2f06392c752dc479d0
singularian <gigagoth@gmail.com>
2018-01-03 20:21:55 -0500

    Updating mdnewdb.go

--- code/encode/mdnewdb.go
+++ code/encode/mdnewdb.go
@@ -13,20 +13,14 @@ func main() {
         var argsNumber int = len(os.Args)
         // var mdname = os.Args[0]
 
-        // sqlite3 db file signature generator
+        // create a new sqlite3 db file signature with the argument filename
 	if argsNumber == 2 {
                 filename := os.Args[1]
-                /* if _, err := os.Stat(filename); os.IsNotExist(err) {
-                        ("The mdfile db does not exist")
-                        os.Exit(3)
-                }*/
-                // fdata := fileData{}
-                // fdata.md = mdFormatSQL.Init(0, filename, "", 0, 0, 0, filename)
-                // fdata.md.InitFile()
-		// fdata.md.EncodeEndFile(format)
+
 		md := mdFormatSQL.Init(0, filename, "", 0, 0, 0, "", "", filename)
 		md.InitFile()
 		md.EncodeEndFile(0)
+	// create a db file signature with the default name
         } else {
 		/* fdata := fileData{}
                 fdata.md = mdFormatSQL.Init(0, filename, "", 0, 0, 0, defaultDBname)
