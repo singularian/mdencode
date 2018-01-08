f5e25a6295f49812ef92a79fd113c7e7f0215e8f
singularian <gigagoth@gmail.com>
2018-01-07 19:01:25 -0500

    Updating md.sh to include a filename check

--- examples/md.sh
+++ examples/md.sh
@@ -8,5 +8,10 @@ FILE="$1"
 EXT=".md"
 OUTPUTFILENAME="$FILE$EXT"
 
+if [ $# -ne 1 ]; then
+    echo $0: usage: $0 filename
+    exit 1
+fi
+
 md -file=$FILE -block=100 -mod=128 -format=4000 -fh=10000111 -bh=10001111 -out=$OUTPUTFILENAME
  
