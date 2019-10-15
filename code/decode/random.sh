b10fbd27cfa470403978ae5339b34dc8e4b2e9e3
singularian <gigagoth@gmail.com>
2019-10-14 20:30:52 -0400

    Updating random.sh

--- code/decode/random.sh
+++ code/decode/random.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
 
-head -c 103 /dev/urandom > randomfile
+RANDOM_FILE="randomfile"
 
+echo "Creating a random file $RANDOM_FILE"
+head -c 103 /dev/urandom > $RANDOM_FILE 
+
+echo ""
+echo "Zipping the file $RANDOM_FILE"
 ./mdzip -file=randomfile -mod=64 -block=11 -out=randomfile.mdz
+
+echo ""
+echo "mdlist $RANDOM_FILE" 
+./mdlist randomfile.mdz
+
+echo ""
+echo "mdunzip $RANDOM_FILE"
 ./mdunzip -file=randomfile.mdz -out=randomfile.mdz.out -thread=16
