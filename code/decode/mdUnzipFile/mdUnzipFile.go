1ece6ecc91c28fbcbcaa4ad158f41a71668656b4
singularian <gigagoth@gmail.com>
2019-12-10 02:59:28 -0500

    Updating mdUnzipFile

--- code/decode/mdUnzipFile/mdUnzipFile.go
+++ code/decode/mdUnzipFile/mdUnzipFile.go
@@ -1,9 +1,11 @@
 package mdUnzipFile
 
-// mdencode
-//
 // mdUnzipFileWG
+// this package unzips and decodes mdzip files with block signatures and modular floors
+//
 // copyright (C) Scott Ross 2017
+// 
+// https://github.com/singularian/mdencode
 // https://github.com/singularian/mdencode/blob/master/LICENSE
 
 import (
@@ -16,8 +18,9 @@ import (
         "math/big"
         "time"
 	"sync"
-        "github.com/singularian/mdencode/code/decode/modScanFileMutex"
+        "github.com/singularian/mdencode/code/decode/mdBlockSize"
 	"github.com/singularian/mdencode/code/decode/mdUnzipFileMutex"
+        "github.com/singularian/mdencode/code/decode/modScanFileMutex"
 )
 
 
@@ -79,7 +82,8 @@ func Init() (md *FileData) {
 	return mdata
 }
 
-// DecodeFile
+// DecodeFile decodes and mdzip file and writes the decoded blocks to an output file
+// it runs a modulus scan on the signature group
 func (l *FileData) DecodeFile(inputFile string, outputFile string, threadCount uint64) int {
 
 	file, err := os.Open(inputFile)
@@ -152,9 +156,12 @@ func (l *FileData) DecodeFile(inputFile string, outputFile string, threadCount u
         blocklist := hashlist[1]
         fmt.Println("hashlist ", filelist, blocklist)
 
+	// initialize the mdBlockSize object
+	mdBlock := mdBlockSize.Init()
+
+	// calculate and return the file signature list byte block size
 	var filelistarr []int
-        //////// filesize, filelistarr = CalcHashSizeFile(filelist)
-        _, filelistarr = l.CalcHashSizeFile(filelist)
+	_, filelistarr = mdBlock.CalcHashBlockSize(filelist)
 
         // fmt.Println("test array ", filelistarr)
         st := strings.Split(filelist, ":")
@@ -164,13 +171,17 @@ func (l *FileData) DecodeFile(inputFile string, outputFile string, threadCount u
                 end = end + uint64(filelistarr[i])
                 var hexstring = fmt.Sprintf("%x", string(bytes[start:end]))
                 // filePathLen   := binary.BigEndian.(bytes[:40])
-                fmt.Println("hashlistname ", st[i], " hex ", hexstring)
-                fmt.Println("hashlistname ", st[i], " hex ", hexstring)
-                ////// mdfmt.EncodeFileHash(format, st[i], hexstring)
+                fmt.Println("file hashlistname ", st[i], " hex ", hexstring)
         }
 
-	// get the file block list
-        _, blocklistarr := l.CalcHashSizeFile(blocklist)
+	// calculate and return the file signature block byte size list
+	// mdblocksize, blocklistarr := mdBlock.CalcHashBlockSize(blocklist)
+	_, blocklistarr := mdBlock.CalcHashBlockSize(blocklist)
+
+	// calculate the file blocks count and the last file block size
+	// ie if the file size is 100 and block size is 12 calculate the number of blocks and
+	// remainder
+	// 100 byte file contains 9 blocks with the last block 4 bytes
         blocks, remainder := l.calculateFileBlocks(fileSize, blockSize)
 
 	// create the mutex
@@ -291,82 +302,8 @@ func (l *FileData) Convert64(data []byte) (uint64, error) {
         return v, nil
 }
 
-
-func (l *FileData) CalcHashSizeFile (hashlist string) (uint64, []int) {
-        st := strings.Split(hashlist, ":")
-
-        var blocksize uint64 = 0
-        var s []int
-        // var s = make([]int, 100)
-
-        // s = append(s, 5)
-
-        for i := 0; i < len(st); i++ {
-                // fmt.Println("hashlist ", st[i])
-
-                switch st[i] {
-                        case "blake2":
-                        s = append(s, 64)
-                        case "blake2b":
-                        s = append(s, 64)
-                        case "blake2s_128":
-			s = append(s, 16) 
-                        case "blake2_256":
-			s = append(s, 32) 
-			case "murmur3":
-			s = append(s, 16)
-			case "md4":
-			s = append(s, 16)
-			case "md5":
-			s = append(s, 16)
-			case "ripe160":
-			s = append(s, 20)
-			case "sha1":
-			s = append(s, 20)
-			case "sha224":
-			s = append(s, 28)
-			case "sha256":
-			s = append(s, 32)
-                        case "sha512":
-                        s = append(s, 64)
-                        case "sha512_224":
-                        s = append(s, 28)
-                        case "sha512_256":
-                        s = append(s, 32)
-                        case "sha512_384":
-                        s = append(s, 48)
-                        case "sha3_224":
-                        s = append(s, 28)
-                        case "sha3_256":
-                        s = append(s, 32)
-                        case "skein_160":
-                        s = append(s, 20)
-                        case "skein_224":
-                        s = append(s, 28)
-                        case "skein_256":
-                        s = append(s, 32)
-                        case "skein_384":
-                        s = append(s, 48)
-                        case "skein_512":
-                        s = append(s, 64)
-                        case "skein_1024":
-                        s = append(s, 128)
-                        case "tiger":
-                        s = append(s, 48)
-                        case "whirlpool":
-                        s = append(s, 64)
-                }
-
-        }
-
-        for i := range s {
-                blocksize += uint64(s[i])
-        }
-
-	 return blocksize, s
-}
-
-// calculate the number of file blocks
+// calculate the number of file blocks 
+// calculate the size of the last file block remainder
 func (l *FileData) calculateFileBlocks(fileSize uint64, blockSize uint64) (uint64, uint64) {
 
 	remainder := fileSize % blockSize
