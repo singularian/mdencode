f50ef8ef8968e7589c7a534649c043bcb626ea96
singularian <gigagoth@gmail.com>
2019-12-15 02:39:03 -0500

    Updating mdBlockSize/mdBlockSize.go to make it cleaner

--- code/decode/mdBlockSize/mdBlockSize.go
+++ code/decode/mdBlockSize/mdBlockSize.go
@@ -2,7 +2,7 @@ package mdBlockSize
 
 import (
 	"strings"
-	"fmt"
+//	"fmt"
 )
 
 // BlockSize object stores the file block signature size
@@ -27,9 +27,6 @@ func Init() (bl *BlockSize) {
 func (bl *BlockSize) CalcHashBlockSize (hashlist string) (uint64, []int) {
         hashName := strings.Split(hashlist, ":")
 
-        var blocksize uint64 = 0
-        var s []int
-
 	// clear this since the file block is also using it to calculate the file block size
 	// it needs this for the whole file signature size for mdunzip and the file block signature size
 	bl.ClearBlockSig()
@@ -38,119 +35,81 @@ func (bl *BlockSize) CalcHashBlockSize (hashlist string) (uint64, []int) {
 	bl.hashList = hashlist
 
         for i := 0; i < len(hashName); i++ {
-                // fmt.Println("hashlist ", st[i])
+                // fmt.Println("hashlist ", hashName[i])
 
                 switch hashName[i] {
                         case "blake2":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
                         case "blake2b":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
                         case "blake2s_128":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
                         case "blake2s_256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
 			case "fnv":
-				s = append(s, 8)
 				bl.AddBlockSig(8)
 			case "hmac256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
 			case "hmac512":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
 			case "kekkak":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
                         case "murmur3":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
                         case "md4":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
                         case "md5":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
                         case "ripe160":
-				s = append(s, 20)
 				bl.AddBlockSig(20)
 			case "sha1_128":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
 			case "sha1_1284":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
 			case "sha1":
-				s = append(s, 20)
 				bl.AddBlockSig(20)
                         case "sha224":
-				s = append(s, 28)
 				bl.AddBlockSig(28)
                         case "sha256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
                         case "sha512":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
                         case "sha512_224":
-				s = append(s, 28)
 				bl.AddBlockSig(28)
                         case "sha512_256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
                         case "sha3_224":
-				s = append(s, 28)
 				bl.AddBlockSig(28)
                         case "sha3_256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
 			case "sha3_384":
-				s = append(s, 48)
 				bl.AddBlockSig(48)
 			case "sha3_512":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
 			case "siphash":
-				s = append(s, 16)
 				bl.AddBlockSig(16)
                         case "skein_160":
-				s = append(s, 20)
 				bl.AddBlockSig(20)
                         // case "skein_224":
-			// 	s = append(s, 28)
+			//	bl.AddBlockSig(28)
                         case "skein_256":
-				s = append(s, 32)
 				bl.AddBlockSig(32)
                         case "skein_384":
-				s = append(s, 48)
 				bl.AddBlockSig(48)
                         case "skein_512":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
                         case "skein_1024":
-				s = append(s, 128)
 				bl.AddBlockSig(128)
                         case "tiger":
-				s = append(s, 24)
 				bl.AddBlockSig(24)
                         case "whirlpool":
-				s = append(s, 64)
 				bl.AddBlockSig(64)
                 }
 
         }
 
-        for i := range s {
-                blocksize += uint64(s[i])
-        }
-
-	// fmt.Println("size ", blocksize, s)
 	// fmt.Println("size 2222 ",  bl.blockSize, bl.blockSizeArr)
 
-        // return blocksize, s
 	return bl.blockSize, bl.blockSizeArr
 }
 
