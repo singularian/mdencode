0a0f09189bc064ad3262eb02a8d55c263ca22f64
singularian <gigagoth@gmail.com>
2019-12-30 22:12:14 -0500

    Updating mdBinaryList.go to cleanup the GetHashList function

--- code/decode/cmd/mdsig.go
+++ code/decode/cmd/mdsig.go
@@ -13,18 +13,19 @@ func main() {
 
 
 	mdl := mdBinaryList.Init()
-	// hlist := mdl.GetHashList()
+/* 
+	hlist := mdl.GetHashList()
 
-        // for i := 0; i < len(hlist); i++ {
-        //        fmt.Printf("%3d: %s\n", i+1, hlist[i])
-        // }
+	for i := 0; i < len(hlist); i++ {
+		number := fmt.Sprintf("%d:", i+1)
+		fmt.Printf("%-3s %s\n", number, hlist[i])
+	}
+*/
 
 	hlistObject := mdl.GetHashListObject()
 	for i := 0; i < len(hlistObject); i++ {
-		
 		number := fmt.Sprintf("%d:", i+1)
 		fmt.Printf("%-3s %-11s - %s\n", number, hlistObject[i].HashName,  hlistObject[i].Description)
-		// fmt.Printf("%3d: %s\n", i+1, hlistObject[i].Description)
 	}
 
 }
