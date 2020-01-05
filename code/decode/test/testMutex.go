ca6eb368b992952f398b9dc8ee4e275ab20f679a
singularian <gigagoth@gmail.com>
2020-01-05 01:16:51 -0500

    Updating testMutex.go

--- code/decode/test/testMutex.go
+++ code/decode/test/testMutex.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"encoding/hex"
 	"github.com/singularian/mdencode/code/decode/mdUnzipMutex"
-	// "github.com/singularian/mdencode/code/decode/mdBlockSize"
-	"github.com/singularian/mdencode/code/decode/mdBinaryList"
-	"github.com/singularian/mdencode/code/decode/mdHashContextList"
 )
 func main() {
 
 	// test the file mutex
 	b := []byte{1, 2, 3, 0, 10, 0, 11, 22}
-	c := []byte{1, 2, 3, 0, 10, 0, 11, 22, 1, 1, 1}
 	mutex := mdUnzipMutex.Init()
 	mutex.SetFileBuffer(1, b)
 	fmt.Println("mutex ", mutex.GetMatchStatus(), " ", mutex.GetFileBuffer())
