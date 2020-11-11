b5196ea08eba209a1c98319a680ca30cbc7555ef
singularian <gigagoth@gmail.com>
2020-11-11 17:11:02 -0500

    Updating decoderRandomTestHCthreads.cpp to increase the signatures to 12

--- code/decode_cpp/decoderRandomTestHC.cpp
+++ code/decode_cpp/decoderRandomTestHC.cpp
@@ -46,7 +46,7 @@ int main (int argc, char **argv) {
      int threadnumber = 0;
      int threadcount  = 8;
      // current signature number handled in the hash context list
-     int signum       = 11;
+     int signum       = 12;
 
      // process the command line argument with the CLI11 command line parser
      CLI::App app{"MDEncode GMP C++ Test Program"};
