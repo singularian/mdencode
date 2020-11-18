0950754811270fa1e5dfae4187876452254ec9b3
singularian <gigagoth@gmail.com>
2020-11-18 18:55:56 -0500

    Updating the mdMutexLog.h to remove the spdlog code

--- code/decode_cpp/mdMutexLog.h
+++ code/decode_cpp/mdMutexLog.h
@@ -3,8 +3,6 @@
 #include <vector>
 #include <mutex>
 #include <time.h>
-//#include "spdlog/spdlog.h"
-//#include "spdlog/sinks/basic_file_sink.h"
 #include <plog/Log.h>
 #include "plog/Initializers/RollingFileInitializer.h"
 
@@ -15,8 +13,6 @@ private:
     int lastThread;
     int threadcount;
     int blocksize;
-    // std::shared_ptr<spdlog::logger> log; 
-    // std::shared_ptr* log; 
     std::mutex mutex;
 public:
     std::string logger_name;
@@ -36,16 +32,6 @@ public:
        // plog::init(plog::info, (const char*) logfilename);
          plog::init(plog::debug, (const char*) logfilename);
          PLOGD << "starting log!"; 
-
-       // if (islogging) {
-          // auto log = spdlog::basic_logger_mt("mdencode_logger", "logs/basic-log.txt");
-          //auto log = spdlog::basic_logger_mt("mdencode_logger", logger_name);
-          // registerLogger(log);
-          // auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
-          // auto log = spdlog::basic_logger_mt("mdencode_logger", logfilename); 
-          //log->warn("testlog");
-          //spdlog::set_default_logger(log);
-       }
     }
 
     // Destructor
@@ -103,7 +89,7 @@ public:
     // void writeLogThread(int thread, mpz_t blockint, char *logtext) 
     void writeLogThread(int thread, mpz_t blockint) 
     {
-        char blockInt [1000]; // need to make this fit the byteblock bigint size
+        char blockInt [2000]; // need to make this fit the byteblock bigint size
         if (islogging) {
            mutex.lock();
            lastThread = thread;
@@ -125,6 +111,6 @@ public:
       time ( &rawtime );
       timeinfo = localtime ( &rawtime );
 
-      sprintf(output, "logs/decodeRandom_%d%d%d%d%d%d.log",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+      sprintf(output, "decodeRandom_%d%d%d%d%d%d.log",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
     }
 };
