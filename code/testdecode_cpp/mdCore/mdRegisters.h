a88ffe6e2b1a05af566e29f04817993b13930eca
singularian <gigagoth@gmail.com>
2021-01-24 22:39:18 -0500

    Updating mdRegisters.h to make the seed more consistent

--- code/testdecode_cpp/mdCore/mdRegisters.h
+++ code/testdecode_cpp/mdCore/mdRegisters.h
@@ -5,22 +5,22 @@ struct hash_list
     // city hash
     uint64_t city64i;
     uint64_t city64o;
-    uint64_t city64seed = 102922;
+    uint64_t city64seed = 100000;
     // crc32
-    uint32_t crc32seed = 12121221;
+    uint32_t crc32seed  = 100000;
     uint32_t crc32i;
     uint32_t crc32o;
     // crc64
-    uint64_t crc64seed = 12121221;
+    uint64_t crc64seed  = 100000;
     uint64_t crc64i;
     uint64_t crc64o;
     // fasthash
     uint32_t fast32i;
     uint32_t fast32o;
-    uint32_t fast32seed = 200;
+    uint32_t fast32seed = 100000;
     uint64_t fast64i;
     uint64_t fast64o;
-    uint64_t fast64seed = 1;
+    uint64_t fast64seed = 100000;
     // fnv1
     Fnv32_t fnv32_1i; 
     Fnv32_t fnv32_1o;
@@ -37,10 +37,10 @@ struct hash_list
     // metro64hash
     uint8_t met641i[8];
     uint8_t met641o[8];
-    uint32_t met641seed = 1237789;
+    uint32_t met641seed = 100000;
     uint8_t met642i[8];
     uint8_t met642o[8];
-    uint32_t met642seed = 1237789;
+    uint32_t met642seed = 100000;
     // md2 to md6
     uint8_t md2i[16];
     uint8_t md2o[16];
@@ -57,18 +57,18 @@ struct hash_list
     // mx3
     uint64_t mx3i;
     uint64_t mx3o;
-    uint64_t mx3seed = 121212;
+    uint64_t mx3seed    = 100000;
     // pengyhash
     uint64_t png64i;
     uint64_t png64o;
-    uint32_t png64seed = 127982;
+    uint32_t png64seed  = 100000;
     // ripe 160
     uint8_t ripe160i[21];
     uint8_t ripe160o[21];
     // seahash
     uint64_t sea64i;
     uint64_t sea64o;
-    uint64_t sea64seed = 12312;
+    uint64_t sea64seed  = 100000;
     // sha1 family
     uint8_t sha1i[21];
     uint8_t sha1o[21];
@@ -92,53 +92,53 @@ struct hash_list
     // xxhash
     uint32_t xxhash32i;
     uint32_t xxhash32o;
-    uint32_t xxseed32 = 0;
+    uint32_t xxseed32 = 100000;
     uint64_t xxhash64i;
     uint64_t xxhash64o;
-    uint64_t xxseed64 = 0;
+    uint64_t xxseed64 = 100000;
     // Whirlpool
     uint8_t whp512i[64];
     uint8_t whp512o[64];
     // wyhash
     uint64_t wyhash64i;
     uint64_t wyhash64o;
-    uint64_t wyseed64 = 10232123120;
+    uint64_t wyseed64 = 100000;
     uint64_t wysecret64[5] = {0xa0761d6478bd642full, 0xe7037ed1a0b428dbull, 0x8ebc6af09c88c6e3ull, 0x589965cc75374cc3ull, 0x1d8e4e27c47d124full};
 };
 
 struct hash_key_list
 {
     // city hash
-    uint64_t city64seed = 102922;
+    uint64_t city64seed = 100000;
     // crc32
-    uint32_t crc32seed = 12121221;
+    uint32_t crc32seed = 100000;
     // crc64
-    uint64_t crc64seed = 12121221;
+    uint64_t crc64seed = 100000;
     // fasthash
-    uint32_t fast32seed = 200;
-    uint64_t fast64seed = 1;
+    uint32_t fast32seed = 100000;
+    uint64_t fast64seed = 100000;
     // highway hash
     const uint64_t hw64key[4] = {1, 2, 3, 4};
     // metro64hash
-    uint32_t met641seed = 1237789;
-    uint32_t met642seed = 1237789;
+    uint32_t met641seed = 100000;
+    uint32_t met642seed = 100000;
     // md6
     unsigned char md62key[16] = {0,1,2,3,4,5,6,7,8,9,0xa,0xb,0xc,0xd,0xe,0xf};
     // mx3
-    uint64_t mx3seed = 121212;
+    uint64_t mx3seed    = 100000;
     // pengyhash
-    uint32_t png64seed = 127982;
+    uint32_t png64seed  = 100000;
     // seahash
-    uint64_t sea64seed = 12312;
+    uint64_t sea64seed  = 100000;
     // siphash
     char sipkey[16] = {0,1,2,3,4,5,6,7,8,9,0xa,0xb,0xc,0xd,0xe,0xf};
     // spooky 32 / 64
-    uint32_t spookyseed32 = 12312;
-    uint64_t spookyseed64 = 1232;
+    uint32_t spookyseed32 = 100000;
+    uint64_t spookyseed64 = 100000;
     // xxhash
-    uint32_t xxseed32 = 0;
-    uint64_t xxseed64 = 0;
+    uint32_t xxseed32 = 100000;
+    uint64_t xxseed64 = 100000;
     // wyhash
-    uint64_t wyseed64 = 10232123120;
+    uint64_t wyseed64 = 100000;
     uint64_t wysecret64[16] = {0,1,2,3,4,5,6,7,8,9,0xa,0xb,0xc,0xd,0xe,0xf};
 };
