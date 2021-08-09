1b9137caeff3d5b39ecfb07d9891ba43e1839988
singularian <gigagoth@gmail.com>
2021-08-09 16:47:33 -0400

    Updating /code/mdzip_go/cmd/randomFile2.go to add arguments for zero padding a block and setting the first nonzero byte

--- code/mdzip_go/cmd/randomFile2.go
+++ code/mdzip_go/cmd/randomFile2.go
@@ -27,7 +27,10 @@ var (
 
 // mdencode flag struct
 type FlagData struct {
-        blocksize uint64 
+        blocknum  uint64
+        blocksize uint64
+        zerosize  uint64
+        firstbyte uint64
         // output file name
         outputfilename string
 }
@@ -45,26 +48,33 @@ func createRandFile() int {
 	var argsNumber int = len(os.Args)
 
 	fd := new(FlagData)
-	flag.Uint64Var(&fd.blocksize, "size", 100, "Random File Block Bytes Size")
+	flag.Uint64Var(&fd.blocknum, "blocknum", 100, "Random File Blocks Number")
+	flag.Uint64Var(&fd.blocksize,"bsize",    14,  "Random File Blocks Bytes Size")
+	flag.Uint64Var(&fd.zerosize, "zsize",    4,   "Random File Blocks Zero Bytes Size")
+	flag.Uint64Var(&fd.firstbyte,"first",    1,   "Random File First Block Byte")
 	flag.StringVar(&fd.outputfilename, "out", "", "Output Filename")
 
 	flag.Usage = printUsage
 
 	flag.Parse()
 
-	if argsNumber != 3 {
+	if argsNumber != 6 {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	bs := []byte{0,0,0,0,0}
-
 	var a uint64 = 0
+	var b uint64 = 0
 	// actually just create a bunch of random file blocks
-	for a = 0; a < fd.blocksize; a++ {
-		fd.WriteFile(bs)
-		bytes, _ := sigRand.GenerateRandomBytes64(9)
-		bytes[0] = 20;
+	for a = 0; a < fd.blocknum; a++ {
+		fmt.Println("Generating block ", a + 1)
+		bytes, _ := sigRand.GenerateRandomBytes64(fd.blocksize)
+		for b = 0; b < fd.zerosize; b++ {
+			bytes[b] = 0;
+		}
+		if fd.zerosize > 1 {
+			bytes[b-1] = uint8(fd.firstbyte)
+		}
 		fd.WriteFile(bytes)
 	}
 
@@ -76,7 +86,7 @@ func createRandFile() int {
 // Write the random file bytes
 func (l *FlagData) WriteFile(bytes []byte) {
 
-	outf, err := os.OpenFile(l.outputfilename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) 
+	outf, err := os.OpenFile(l.outputfilename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600) 
 	if err != nil {
 		fmt.Println("Counldn't open output file ", err)
 		os.Exit(1)
@@ -92,10 +102,17 @@ func printUsage() {
 
 	// prints the flag arguments in non sorted order
 	fmt.Println(`
-  -size uint64
+  -blocknum uint64
        Random File Bytes Size (default "100")
+  -bsize uint64
+       Random File Blocks Bytes Size (default "14")
+  -zsize uint64
+       Random File Blocks Zero Bytes Size (default "4")
   -out string
         Output Filename
+
+   Examples:
+	./rfblock -blocknum=10 -bsize=14 -zsize=5 -first=14 -out=randfile
     `)
 
 	fmt.Println()
